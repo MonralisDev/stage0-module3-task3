362e63223427ce5a2697a3871af4f98dfbe6a1c9
M <M>
2025-01-25 12:41:12 +0200

    ++

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int a = 1;
+        int b = 10;
+        int c = 100;
+
+        System.out.println(a + "\n" + b + "\n" + c);
     }
 }
